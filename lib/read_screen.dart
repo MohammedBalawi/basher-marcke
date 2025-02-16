1337b2e0c17acb1989bdcff4ad4896259bcf2d0a
Mohammed Balawi <bbmm6066@gmail.com>
2025-02-16 16:28:39 +0200

    Fix GitHub Actions build

--- lib/read_screen.dart
+++ lib/read_screen.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:ui';
 import 'package:bashar_market/shared_pref/shared.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:barcode_scan2/barcode_scan2.dart';
 import 'package:excel/excel.dart';
